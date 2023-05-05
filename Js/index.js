3b7d75d54b2656218bb08a340118d615243debfa
BryCamp <118139968+BryCamp@users.noreply.github.com>
2023-05-04 22:34:18 -0600

    UX Homework 19 JS

---
+++ Js/index.js
@@ -0,0 +1,7 @@
+
+
+  document.getElementById("section2-button").addEventListener("click", function() {
+    window.location.href = "#section2";
+    console.log("You clicked on my work");
+  });
+ 
